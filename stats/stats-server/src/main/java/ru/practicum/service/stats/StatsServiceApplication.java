bb8bed2bd5ae13d88809b922a88e10a679746d26
Leonid Solovev <leonidkamka353@yandex.ru>
2025-12-02 01:59:18 +0500

    Первый коммит с реализацией технического задания 1 этапа дипломного проекта.

--- stats/stats-server/src/main/java/ru/practicum/service/stats/StatsServiceApplication.java
+++ stats/stats-server/src/main/java/ru/practicum/service/stats/StatsServiceApplication.java
@@ -2,10 +2,11 @@ package ru.practicum.service.stats;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ru.practicum.service.stats.client.config.FeignConfig.class)
 public class StatsServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StatsServiceApplication.class, args);
     }
--- stats/stats-server/src/main/java/ru/practicum/service/stats/controller/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/service/stats/controller/StatsController.java
@@ -1,43 +1,32 @@
 package ru.practicum.service.stats.controller;
 
-import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-import ru.practicum.service.stats.StatsDtoResponse;
-import ru.practicum.service.stats.service.StatsService;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.service.stats.GetStatsRequest;
 import ru.practicum.service.stats.StatsDtoRequest;
+import ru.practicum.service.stats.StatsDtoResponse;
+import ru.practicum.service.stats.client.StatsClient;
+import ru.practicum.service.stats.service.StatsService;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
 @RestController
-public class StatsController {
+@RequiredArgsConstructor
+public class StatsController implements StatsClient {
     final StatsService statsService;
 
-    @Autowired
-    public StatsController(StatsService statsService) {
-        this.statsService = statsService;
-    }
-
-    @PostMapping("/hit")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void hit(@RequestBody StatsDtoRequest request) {
+    @Override
+    public void hit(StatsDtoRequest request) {
         log.info("Запрос на добавление данных в статистику");
         statsService.saveHit(request);
     }
 
-    @GetMapping("/stats")
-    @ResponseStatus(HttpStatus.OK)
-    public List<StatsDtoResponse> findStats(
-            @RequestParam("start") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @NotNull LocalDateTime start,
-            @RequestParam("end") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") @NotNull LocalDateTime end,
-            @RequestParam(name = "uris", required = false) List<String> uris,
-            @RequestParam(name = "unique", defaultValue = "false") Boolean unique) {
+    @Override
+    public List<StatsDtoResponse> findStats(LocalDateTime start, LocalDateTime end,
+                                            List<String> uris, Boolean unique) {
         log.info("Запрос на получение статистики");
         log.info("Параметры: \nstart = {} \nend = {} \nuris = {} \nunique = {}", start, end, uris, unique);
         return statsService.findStats(GetStatsRequest.of(start, end, uris, unique));
